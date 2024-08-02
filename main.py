7bb0e0f9ebdbc3e93145016f06c2ec8a820dea72
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-08-01 21:30:46 -0400

    mouse and keyboard release actions

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pynput import mouse, keyboard
 def on_click(x, y, button, pressed):
     if pressed and button == mouse.Button.left:
         print('M1 pressed')
+    elif not pressed:
+        print(button, 'released')
 
 
 def on_press(key):
@@ -13,8 +15,12 @@ def on_press(key):
         print('special key {0} pressed'.format(
             key))
 
+def on_release(key):
+    print('{0} released'.format(
+        key))
 
-keyboard_listener = keyboard.Listener(on_press=on_press)
+
+keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 mouse_listener = mouse.Listener(on_click=on_click)
 
 # Start threads
@@ -24,3 +30,6 @@ mouse_listener.start()
 # Keep threads running until program is terminated with ctrl + c
 keyboard_listener.join()
 mouse_listener.join()
+
+# keep track of how long each button is held down for
+# store this in persistent file, different file for each script session
